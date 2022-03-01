e6a2472c588ec0cdae10a25016099f2a0a483653
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2022-03-01 19:37:32 +0000

    Added Error Reducer

---
+++ unison/src/redux/reducers/AuthErrorReducer.js
@@ -0,0 +1,19 @@
+const { AuthActionType } = requice("../actions/AuthAction");
+
+const authError = {
+    message: "",
+};
+
+const authErrorReducer = (state = authError, action) => {
+    switch (action.type) {
+        case AuthActionType.REGISTER_FAIL:
+            return { message: action.payload };
+        case AuthActionType.LOGOUT_FAIL:
+            return { message: action.payload };
+        case AuthActionType.LOGIN_FAIL:
+            return { message: action.payload };
+        default: return state;
+    }
+};
+
+export default authErrorReducer;
