e641757ed77d9a3fe02ceae90fa3118b213938c4
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2022-03-01 19:33:02 +0000

    Redux actions added

--- unison/src/redux/actions/AuthAction.js
+++ unison/src/redux/actions/AuthAction.js
@@ -1,10 +1,71 @@
 import acios from "axios";
 
 const AuthActionType = {
-    REGISTER_SUCCESS: "REGISTER_SUCCESS",
-    REGISTER_FAIL: "REGISTER_FAIL",
-    LOGOUT_SUCCESS: "LOGOUT_SUCCESS",
-    LOGOUT_FAIL: "LOGOUT_FAIL",
-    LOGIN_SUCCESS: "LOGIN_SUCCESS",
-    LOGIN_FAIL: "LOGIN_FAIL",
+  REGISTER_SUCCESS: "REGISTER_SUCCESS",
+  REGISTER_FAIL: "REGISTER_FAIL",
+  LOGOUT_SUCCESS: "LOGOUT_SUCCESS",
+  LOGOUT_FAIL: "LOGOUT_FAIL",
+  LOGIN_SUCCESS: "LOGIN_SUCCESS",
+  LOGIN_FAIL: "LOGIN_FAIL",
 };
+
+const RegisterAuthAction = (userState, history, setErrorHandler) => {
+  return async (dispatch) => {
+    try {
+      const res = await axios.post("/register", userState);
+      const { data } = res;
+      dispatch({ type: AuthActionType.REGISTER_SUCCESS, payload: data });
+      history.push("/login");
+    } catch (error) {
+      if (error.response) {
+        dispatch({
+          type: AuthActionType.REGISTER_FAIL,
+          payload: error.response.data.message,
+        });
+        setErrorHandler({
+          hasError: true,
+          message: error.response.data.message,
+        });
+      }
+    }
+  };
+};
+
+const LoginAuthAction = (loginState, history, setErrorHandler) => {
+  return async (dispatch) => {
+    try {
+      const res = await axios.post("/authenticate", loginState);
+      const { data } = res;
+      dispatch({ type: AuthActionType.LOGIN_SUCCESS, payload: data });
+      history.push("/timeline");
+    } catch (error) {
+      if (error.response) {
+        dispatch({
+          type: AuthActionType.LOGIN_FAIL,
+          payload: error.response.data.message,
+        });
+        }
+        setErrorHandler({ hasError: true, message: error.response.data.message });
+    }
+  };
+};
+
+const LogOutAuthAction = (history) => {
+    return async (dispatch) => {
+        try {
+            const res = await axios.get("/logout");
+            const { data } = res;
+            dispatch({
+                type: AuthActionType.LOGOUT_SUCCESS,
+                payload: data.message,
+            });
+            history.push("/login")
+        } catch (error) {
+            if (error.response) {
+                dispatch({ typetype: AuthActionType.LOGIN_FAIL, payload: error.response.data.message });
+            }
+        }
+    };
+};
+
+export { AuthActionType, RegisterAuthAction, LogOutAuthAction, LoginAuthAction }
