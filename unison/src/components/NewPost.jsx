c04fd4b4f11390f9ac51ebc454461159ee8bbc04
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2022-03-07 16:33:21 +0000

    update

--- unison/src/components/NewPost.jsx
+++ unison/src/components/NewPost.jsx
@@ -1,48 +1,64 @@
 import { Avatar } from "@material-ui/core";
-import VideocamIcon from '@mui/icons-material/Videocam';
-import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary';
-import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import VideocamIcon from "@mui/icons-material/Videocam";
+import PhotoLibraryIcon from "@mui/icons-material/PhotoLibrary";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
 import React, { useState } from "react";
 import "../style/style.css";
 
 function NewPost() {
+  const [input, setInput] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
 
-    const [input, setInput] = useState("");
-    const [imageUrl, setImageUrl] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    
-        setInput("");
-        setImageUrl("");
-  }
+    setInput("");
+    setImageUrl("");
+  };
 
   return (
-      <div className="messageSender">
-         
-          <div className="messageSender__top">
-               <Avatar src="https://media-exp1.licdn.com/dms/image/C4E03AQFjH2M4r3umhQ/profile-displayphoto-shrink_200_200/0/1629117547194?e=1642636800&v=beta&t=4u_D_k0dRgzK84Xu5-2S3DFN-zdierUdEeay9LJv4Wg"/>
-              <form>
-                 
-                  <input value={input} onChange={(e) => setInput(e.target.value)} className="messageSender__input" type="text" placeholder={`What's on your mind, Paul?`}/>
-                   <button onClick={handleSubmit} type="submit">Hidden Submit</button>
-              </form>
-          </div>
-          
-          <div className="messageSender__bottom">
-              <div className="messageSender__option">
-                  <VideocamIcon style={{ color: "red" }} />
-                  <h3>Live Video</h3>
-              </div>
-              <div className="messageSender__option">
-                  <PhotoLibraryIcon style={{ color: "green" }} />
-                  <h3>Photo/Video</h3>
-              </div>
-              <div className="messageSender__option">
-                  <InsertEmoticonIcon style={{ color: "orange" }} />
-                  <h3>Feeling/Activity</h3>
-              </div>
-          </div>
+    <div className="messageSender">
+      <div className="messageSender__top">
+        <Avatar src="https://media-exp1.licdn.com/dms/image/C4E03AQFjH2M4r3umhQ/profile-displayphoto-shrink_200_200/0/1629117547194?e=1642636800&v=beta&t=4u_D_k0dRgzK84Xu5-2S3DFN-zdierUdEeay9LJv4Wg" />
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className="messageSender__input"
+            type="text"
+            placeholder={`What's on your mind, Paul?`}
+          />
+          <button onClick={handleSubmit} type="submit">
+            Hidden Submit
+          </button>
+        </form>
+      </div>
+
+      <div className="messageSender__bottom">
+        <div className="messageSender__option">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="red"
+            class="bi bi-music-note"
+            viewBox="0 0 16 16"
+          >
+            <path d="M9 13c0 1.105-1.12 2-2.5 2S4 14.105 4 13s1.12-2 2.5-2 2.5.895 2.5 2z" />
+            <path fill-rule="evenodd" d="M9 3v10H8V3h1z" />
+            <path d="M8 2.82a1 1 0 0 1 .804-.98l3-.6A1 1 0 0 1 13 2.22V4L8 5V2.82z" />
+          </svg>
+          <h3>Audio Clip</h3>
+        </div>
+        <div className="messageSender__option">
+          <PhotoLibraryIcon style={{ color: "green" }} />
+          <h3>Photo/Video</h3>
+        </div>
+        <div className="messageSender__option">
+          <InsertEmoticonIcon style={{ color: "orange" }} />
+          <h3>Feeling/Activity</h3>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ unison/src/components/Post.jsx
@@ -0,0 +1,51 @@
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import NearMeIcon from '@mui/icons-material/NearMe';
+import { Avatar } from "@material-ui/core";
+import "../style/style.css";
+import React from "react";
+
+function Post({ profilePic, image, username, timestamp, message }) {
+  return (
+    <div className="post">
+      <div className="post__top">
+        <Avatar src={profilePic} className="post__avatar" />
+        <div className="post__topInfo">
+          <h3>{username}</h3>
+          {timestamp}
+        </div>
+      </div>
+
+      <div className="post__bottom">
+        <p>{message}</p>
+      </div>
+
+      <div className="post__image">
+        <img src={image} alt="" />
+      </div>
+
+      <div className="post__options">
+        <div className="post__option">
+          <ThumbUpIcon />
+          <p>Like</p>
+        </div>
+        <div className="post__option">
+          <ChatBubbleOutlineIcon />
+          <p>Comment</p>
+        </div>
+        <div className="post__option">
+          <NearMeIcon />
+          <p>Share</p>
+        </div>
+        <div className="post__option">
+                  <AccountCircleIcon />
+                  <ExpandMoreIcon />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Post;
