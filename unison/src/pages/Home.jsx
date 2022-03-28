97209106c74ba01b4a8bd415bcc76ea9cc9cbe50
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2022-03-28 10:42:06 +0100

    Update

--- unison/src/pages/Home.jsx
+++ unison/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useSelector, connect, useDispatch } from "react-redux";
-import { addCurrentUserAction } from "../redux/actions/UserAction"
+import { addCurrentUserAction } from "../redux/actions/UserAction";
 import Sidebar from "../components/Sidebar";
 import Feed from "../components/Feed";
 import Rightbar from "../components/Rightbar";
@@ -9,41 +9,41 @@ import NavBar from "../components/Navbar";
 const Home = (props) => {
   const [profile, setProfile] = useState([]);
   const jwtToken = useSelector((state) => state.authState.user.accessToken);
-   const email = useSelector((state) => state.authState.user.email);
+  const email = useSelector((state) => state.authState.user.email);
   console.log("email", email);
-  
+
   console.log(jwtToken);
-  
-  console.log("profile",profile);
+
+  console.log("profile", profile);
 
   const dispatch = useDispatch();
 
-  
-
   useEffect(() => {
     const fetchProfile = async () => {
-    // let token = JSON.parse(localStorage.getItem("auth"));
-    // const jwttoken = token.user.accessToken;
-    try {
-      let response = await fetch(`http://localhost:5000/users/currentUser/${email}`, {
-        headers: {
-          Authorization: `Bearer ${jwtToken}`,
-        },
-      });
-     
-      if (response.ok) {
-        
-        let data = await response.json();
-        console.log(data);
-        setProfile(data);
-        dispatch(addCurrentUserAction({data}))
-        console.log(profile);
+      // let token = JSON.parse(localStorage.getItem("auth"));
+      // const jwttoken = token.user.accessToken;
+      try {
+        let response = await fetch(
+          `http://localhost:5000/users/currentUser/${email}`,
+          {
+            headers: {
+              Authorization: `Bearer ${jwtToken}`,
+            },
+          }
+        );
+
+        if (response.ok) {
+          let data = await response.json();
+          console.log(data);
+          setProfile(data);
+          dispatch(addCurrentUserAction({ data }));
+          console.log(profile);
+        }
+      } catch (error) {
+        console.log(error);
       }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-    fetchProfile()
+    };
+    fetchProfile();
   }, []);
 
   return (
